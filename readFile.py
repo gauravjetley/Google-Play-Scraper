eb01f8eba2518ac50816c157225f1e77cdd50a8a
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-10-18 08:01:38 -0400

    Update readFile.py

--- readFile.py
+++ readFile.py
@@ -1,6 +1,9 @@
-fp=open("file_name.txt")
+fp=open("vr_links_list.txt")
 lines=fp.readlines()
 fp.close()
 
 for line in lines:
-  #new you have each url in each line
+  try:
+    #new you have each url in each line
+  except:
+    pass
